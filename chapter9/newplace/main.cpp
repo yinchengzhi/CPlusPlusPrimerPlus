6eb4b84e5dbc3246f7abc946d51608cdb57c0bfd
Valar Morghulis <524672142@qq.com>
2017-05-28 21:03:08 +0800

    update

--- chapter9/newplace/main.cpp
+++ chapter9/newplace/main.cpp
@@ -4,8 +4,7 @@ const int BUF = 512;
 const int N = 5;
 char buffer[BUF];                           //chunk of memory
 
-int main()
-{
+int main(){
     using namespace std;
     double *pd1,*pd2;
     int i;
@@ -21,5 +20,30 @@ int main()
         cout<<pd1[i]<<" at "<<&pd1[i]<<"; ";
         cout<<pd2[i]<<" at "<<&pd2[i]<<endl;
     }
+
+    cout<<"\nCalling new and placement new a second time:\n";
+    double *pd3,*pd4;
+    pd3 = new double[N];
+    pd4 = new (buffer)double[N];
+    for(i=0;i<N;i++)
+        pd4[i] = pd3[i] = 1000+20.0*i;
+    cout<<"Buffer contents:\n";
+    for(i=0;i<N;i++){
+        cout<<pd3[i]<<" at "<<&pd3[i]<<"; ";
+        cout<<pd4[i]<<" at "<<&pd4[i]<<endl;
+    }
+
+    cout<<"\nCalling new and placement new a third time:\n";
+    delete[] pd1;
+    pd1 = new double [N];
+    pd2 = new (buffer + N*sizeof(double)) double [N];
+
+    for(i=0;i<N;i++){
+        cout<<pd1[i]<<" at "<<&pd1[i]<<"; ";
+        cout<<pd2[i]<<" at "<<&pd2[i]<<endl;
+    }
+
+    delete[] pd1;
+    delete[] pd2;
     return 0;
 }
