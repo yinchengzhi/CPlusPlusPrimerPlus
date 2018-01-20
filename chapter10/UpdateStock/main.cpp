7da6f911ae0f26f29179c2012522a70bebba443b
Valar Morghulis <524672142@qq.com>
2018-01-20 16:37:59 +0800

    update

---
+++ chapter10/UpdateStock/main.cpp
@@ -0,0 +1,9 @@
+// stock1.cpp -- stock class implementation with constructors,destructor added
+#include <iostream>
+#include "stock1.h"
+
+
+int main(){
+    cout << "Hello world!" << endl;
+    return 0;
+}
